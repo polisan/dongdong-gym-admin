970a48840334db6213d4244e8c1e5184ed89e096
deofly <sunkai1911@gmail.com>
2015-02-03 22:41:53 +0800

    修复丢失布局文件Bug

--- components/NeedLoginController.php
+++ components/NeedLoginController.php
@@ -7,6 +7,13 @@ use yii\web\Controller;
 
 class NeedLoginController extends Controller
 {
+    /**
+     * @inheritdoc
+     *
+     * @param \yii\base\Action $action
+     * @return bool|\yii\web\Response
+     * @throws \yii\web\BadRequestHttpException
+     */
     public function beforeAction($action) {
         if (Yii::$app->user->isGuest) {
             return $this->redirect(['passport/account/login']);
